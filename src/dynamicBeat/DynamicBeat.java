9c75e3d0f355be13be89143bb49b1cc3eb0e324e
SangYoo1324 <samuel13241@gmail.com>
2022-01-31 20:59:10 -0800

    1st lesson

--- src/dynamicBeat/DynamicBeat.java
+++ src/dynamicBeat/DynamicBeat.java
@@ -1,7 +1,15 @@
 package dynamicBeat;
 
-public class DynamicBeat {
+import javax.swing.JFrame;
+
+public class DynamicBeat extends JFrame {
 DynamicBeat(){
+	setTitle("DynamicBeat");
+	setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+	setResizable(false);
+	setLocationRelativeTo(null);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setVisible(true);
 	
 }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module Rhythm {
+	requires java.desktop;
 }
