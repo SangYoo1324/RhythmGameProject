d673076a0ffd128d0bb4a179dc060f2ac4e3a745
SangYoo1324 <samuel13241@gmail.com>
2022-02-02 17:02:37 -0800

    background add

--- src/dynamicBeat/DynamicBeat.java
+++ src/dynamicBeat/DynamicBeat.java
@@ -1,8 +1,17 @@
 package dynamicBeat;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class DynamicBeat extends JFrame {
+	private Image screenImage;
+	private Graphics screenGraphic;
+	
+	private Image introBackground;
+	
 DynamicBeat(){
 	setTitle("DynamicBeat");
 	setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
@@ -11,5 +20,18 @@ DynamicBeat(){
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	setVisible(true);
 	
+	introBackground = new ImageIcon(Main.class.getResource("/Images/mainBackground.jpg")).getImage();
+}
+
+public void paint(Graphics g) {
+screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+screenGraphic = screenImage.getGraphics();
+screenDraw(screenGraphic);
+g.drawImage(screenImage,0,0,null);
+}
+
+public void screenDraw(Graphics g) {
+	g.drawImage(introBackground, 0,0,null);
+	//this.repaint();
 }
 }
